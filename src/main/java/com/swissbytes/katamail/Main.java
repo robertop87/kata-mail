fa3bec790d4746ed7e3c7ccf328754a659532421
Luis Roberto Pérez Rios <roberto.perez@swissbytes.ch>
2016-06-03 16:01:29 -0400

    Setup project with Junit 4.12

---
+++ src/main/java/com/swissbytes/katamail/Main.java
@@ -0,0 +1,12 @@
+package com.swissbytes.katamail;
+
+/**
+ * Created by roberto on 3/6/2016.
+ */
+public class Main {
+
+    public static String sayHello() {
+        return "Hello Kata Mail Dojo";
+    }
+}
+
---
+++ src/test/java/com/swissbytes/katamail/MainTest.java
@@ -0,0 +1,16 @@
+package com.swissbytes.katamail;
+
+/**
+ * Created by roberto on 3/6/2016.
+ */
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void testSayHello() {
+        // Religious test
+        assertTrue(true);
+    }
+}
